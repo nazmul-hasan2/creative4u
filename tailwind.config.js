3423a599be3b02eaa82c3b532f3aec3e960f0a81
nazmul-hasan2 <nh5888164@gmail.com>
2024-11-08 11:38:44 +0600

    Header Section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        plus: ['"Plus Jakarta Sans", sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
